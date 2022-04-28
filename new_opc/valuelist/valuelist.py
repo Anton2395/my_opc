1390b1debca5b92bf3e7028ac07edbcd2fcba345
Davidovskii-Nikita <davidovskiinikita@gmail.com>
2022-04-28 18:03:04 +0300

    add update delete area methods

--- new_opc/valuelist/valuelist.py
+++ new_opc/valuelist/valuelist.py
@@ -32,6 +32,17 @@ def list_value_data(id_connection: int) -> str:
 
 @app_value.route("/add_area", methods=["POST"])
 def add_new_area() -> str:
+    """
+    POST URI - http://<server>/value/add_area
+    {
+        "name":<str>,
+        "connection_id":<int>,
+        "area_memory":<str>,
+        "db":<int>,
+        "start":<int>,
+        "size":<int>
+    }
+    """
     try:
         db = _models.Session()
         data_post = request.json
@@ -54,6 +65,17 @@ def add_new_area() -> str:
 
 @app_value.route("/update_area", methods=["PUT"])
 def update_area() -> str:
+    """
+    PUT URI - http://<server>/value/update_area
+    {
+        "name":<str>,
+        "area_id":<int>,
+        "area_memory":<str>,
+        "db":<int>,
+        "start":<int>,
+        "size":<int>
+    }
+    """
     try:
         db = _models.Session()
         data_post = request.json
@@ -66,5 +88,120 @@ def update_area() -> str:
         db.commit()
         db.close()
         return 'seccses'
+    except:
+        return 'error'
+
+@app_value.route("/delete_area", methods=["DELETE"])
+def delete_area() -> str:
+    """
+    DELETE URI - http://<server>/value/delete_area
+    {
+        "area_id":<int>,
+    }
+    """
+    try:
+        data_psot = request.json
+        db = _models.Session()
+        area = db.query(_models.Area).get(data_psot["area_id"])
+        db.delete(area)
+        db.commit()
+        db.close()
+        return 'seccses'
+    except:
+        return 'error'
+
+@app_value.route('/add_value', methods=["POST"])
+def add_value() -> str:
+    """
+    POST URI - http://<server>/value/add_value
+    {
+        "area_id":<int>,
+        "start":<int>,
+        "name_val":<str>,
+        "type_value_get_data":<str>(select "int" or "flot" or "double" or "bool"),
+        "type_value_write_data":<str>(select "int" or "flot" or "double" or "bool"),
+        "if_change":<bool>,
+        "divide":<bool>,
+        "divide_number":<float>,
+        "time_write_if_change":<int>(sec?),
+        "time_rewrite":<int>(min?),
+        "bit":<int>
+    }
+    """
+    data_post = request.json
+    db = _models.Session()
+    new_value = _models.ValueList(
+        area_id=data_post['area_id'],
+        start=data_post['start'],
+        name_val=data_post['name_val'],
+        type_value_get_data=data_post['type_value_get_data'],
+        type_value_write_data=data_post['type_value_write_data'],
+        if_change=data_post['if_change'],
+        divide=data_post['divide'],
+        divide_number=data_post['divide_number'],
+        time_write_if_change=data_post['time_write_if_change'],
+        time_rewrite=data_post['time_rewrite'],
+        bit=data_post['bit'],
+    )
+    db.add(new_value)
+    db.commit()
+    db.close()
+    return 'secces'
+
+
+@app_value.route('/update_value', methods=["PUT"])
+def update_value() -> str:
+    """
+    PUT URI - http://<server>/value/update_value
+    {
+        "value_id":<int>,
+        "start":<int>,
+        "name_val":<str>,
+        "type_value_get_data":<str>(select "int" or "flot" or "double" or "bool"),
+        "type_value_write_data":<str>(select "int" or "flot" or "double" or "bool"),
+        "if_change":<bool>,
+        "divide":<bool>,
+        "divide_number":<float>,
+        "time_write_if_change":<int>(sec?),
+        "time_rewrite":<int>(min?),
+        "bit":<int>
+    }
+    """
+    try:
+        data_post = request.json
+        db = _models.Session()
+        value = db.query(_models.ValueList).get(data_post['value_id'])
+        value.start = data_post['start']
+        value.name_val = data_post['name_val']
+        value.type_value_get_data = data_post['type_value_get_data']
+        value.type_value_write_data = data_post['type_value_write_data']
+        value.if_change = data_post['if_change']
+        value.divide = data_post['divide']
+        value.divide_number = data_post['divide_number']
+        value.time_write_if_change = data_post['time_write_if_change']
+        value.time_rewrite = data_post['time_rewrite']
+        db.commit()
+        db.close()
+        return 'secces'
+    except:
+        return 'error'
+
+
+@app_value.route('/delete_value', methods=['DELETE'])
+def delete_value() -> str:
+    """
+    DELETE URI - http://<server>/value/delete_value
+    {
+        "value_id":<int>,
+    }
+    """
+    try:
+        data_post = request.json
+        db = _models.Session()
+        value = db.query(_models.ValueList).get(data_post['value_id'])
+        db.delete(value)
+        db.commit()
+        db.close()
+        return 'secces'
     except:
         return 'error'
